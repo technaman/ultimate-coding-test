63d457b74089a1aca9c8cff9e7812028b70d07a5
Naman Garg <namangarg.tech@gmail.com>
2022-03-03 18:55:03 +0530

    added basic tests

--- app.test.js
+++ app.test.js
@@ -1,9 +1,17 @@
 const request = require('supertest');
 require('dotenv').config();
 const db = require('./db');
-const makeApp = require('./app');
+const app = require('./app');
+const {getHighestIntent} = require('./util/utils');
+require('dotenv').config();
 
-const app = makeApp(db);
+global.console = {
+    log: jest.fn(),
+    error: jest.fn(),
+    warn: console.warn,
+    info: console.info,
+    debug: console.debug,
+};
 
 beforeAll(async () => {
     await db.connect();
@@ -16,10 +24,70 @@ afterAll(async () => {
 const BOT_ID = '5f74865056d7bb000fcd39ff';
 
 describe('basic sanity checks', () => {
-    test('Returns a 400 for bad request', async () => {
+    test('Returns a 400 for missing parameters', async () => {
         await request(app)
                 .get('/')
                 .query({botId: BOT_ID})
-                .expect(400)
-    })
-})
+                .expect(400);
+
+        await request(app)
+                .get('/')
+                .query({message: 'Hello'})
+                .expect(400);
+    });
+
+    test('Returns a 400 for invalid parameters', async () => {
+        await request(app)
+                .get('/')
+                .query({botId: 'invalidbotid', message: 'Hello'})
+                .expect(400);
+
+        await request(app)
+                .get('/')
+                .query({botId: BOT_ID, message: ''})
+                .expect(400);
+
+    });
+
+    test('Returns a 200 for a valid request', async () => {
+        const response = await request(app)
+            .get('/')
+            .query({botId: BOT_ID, message: 'HELLO'})
+            .expect(200);        
+    })    
+});
+
+describe('Verify correct reply response is sent', () => {
+    test('Returns a correct greeting message from DB for hello', async () => {
+        const reply = await db.getReply('Greeting', 'random test message');
+        const body = {reply};
+        const response = await request(app)
+            .get('/')
+            .query({botId: BOT_ID, message: 'HELLO'});
+
+        expect(response.body).toMatchObject(body);
+    });
+});
+
+describe('Testing Util methods', () => {
+
+    test('Returns a valid number for highest intent', () => {    
+        const intents = [{
+            name: 'Third',
+            confidence: 0.6
+        },{
+            name: 'Second',
+            confidence: 0.7
+        },{
+            name: 'Fourth',
+            confidence: 0.1
+        },{
+            name: 'First',
+            confidence: 0.8
+        }];
+
+        const highestIntent = getHighestIntent(intents);        
+
+        expect(highestIntent).toMatchObject({name: 'First', confidence: 0.8});
+    });
+});
